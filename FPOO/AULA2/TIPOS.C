da6a32e3ddfbf12f41d07c959ce3975dd6264d67
cicero <139743005+Aprendizdechapeleiro@users.noreply.github.com>
2023-11-28 10:06:49 -0300

    Add files via upload

---
+++ FPOO/AULA2/TIPOS.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int x;//variavel -> espa?o de mem?ria
+	x = 'A';
+	printf("Variavel x = %c", x);
+	return 0;
+}
---
+++ FPOO/AULA2/TIPOS2.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	int x = -247483647;//variavel -> espa?o de mem?ria
+	printf("Variavel x = %i", x);
+	return 0;
+}
